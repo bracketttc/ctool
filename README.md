73f627725b9a911d4818c58da6d460935778830b
Timothy Brackett <brackett.tc@gmail.com>
2024-01-07 10:25:02 -0500

    Add working base executable
    Base executable delegates to other commands prefixed by `ctool-`. If the
    exact command name does not exist, it will search for all available
    commands on the current user's PATH or system paths and perform a fuzzy
    match and suggest that to the user (if there's a good enough match). A
    possible option for the future is to automatically run the closest match
    -- I know there have been times where I wished `git` would do that for
    me.

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A CMake wrapper and toolset.
 
 ## Planned tools
 
-- `ctool-autoinstall` - detect missing dependencies and install them using distribution package managers
+- `ctool-depends` - detect missing dependencies and install them using distribution package managers
 - `ctool-changed-tests` - use cmake-file-api and CTest json to determine what files might change the output of which tests
--- src/ctool/__init__.py
+++ src/ctool/__init__.py
@@ -1,19 +1,71 @@
-#!/usr/bin/env python3
-
 """
 ctool
 """
 
+from difflib import get_close_matches
+from shutil import which
+import glob
+import os
+import subprocess
 import sys
+
+from .util import remove_prefix
+
+
+def find_commands():
+    """
+    Discover all ctool commands available
+    """
+    try:
+        paths = os.environ["PATH"].split(os.pathsep)
+    except KeyError:
+        paths = os.defpath.split(os.pathsep)
+    paths = list(set(paths))
+
+    results = []
+    for path_dir in paths:
+        results += glob.glob(os.path.join(glob.escape(path_dir), "ctool-*"))
+
+    commands = [
+        remove_prefix(os.path.basename(result), "ctool-")
+        for result in results
+        if os.access(result, os.X_OK | os.R_OK)
+    ]
+
+    return commands
+
+
+def print_usage():
+    """
+    Print ctool usage
+    """
+    print(
+        "usage: ctool <command> [OPTIONS]\n"
+        "\n"
+        "Known commands:\n"
+        "   changed-tests\tList or run tests changed by a set of files\n"
+        "   depends\t\tList or install project dependencies\n"
+    )
 
 
 def main():
     """
     ctool entry-point
     """
-    print("ctool!")
-    return 0
+    if len(sys.argv) == 1:
+        print_usage()
+        return 0
 
+    command = sys.argv[1]
+    command_exec = which("ctool-" + command)
+    if command_exec is None:
+        print(f"ctool: `{command}` is not an available ctool command.")
 
-if __name__ == "__main__":
-    sys.exit(main())
+        alternate_command = get_close_matches(command, find_commands(), n=1, cutoff=0.7)
+        for cmd in alternate_command:
+            print(f"\nMaybe you meant: `ctool {cmd}`?")
+        return 1
+
+    with subprocess.Popen([command_exec] + sys.argv[2:]) as proc:
+        proc.wait()
+        return proc.returncode
